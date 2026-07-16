ccaa9e568899f28b6b48085f5a8d90085270818d
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-16 15:26:56 -0400

    create SearchBar component

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
 
 export default function Homepage() {
   return (
     <main>
       <Header />
+      <SearchBar />
     </main>
   );
 }
