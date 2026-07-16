c2698a620711f43e88121ef1671afabb7d3c0d98
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-16 18:11:19 -0400

    assign individual images to employees

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,32 +1,42 @@
-import employeeAvatar from "../assets/employee-avatar.png";
 import EmployeeListItem from "./EmployeeListItem";
 
-
+import jamesKing from "../assets/JamesKing.png";
+import julieTaylor from "../assets/JulieTaylor.png";
+import eugeneLee from "../assets/EugeneLee.png";
+import johnWilliams from "../assets/JohnWilliams.png";
+import rayMoore from "../assets/RayMoore.png";
+import paulJones from "../assets/PaulJones.png";
 
 const employees = [
   {
     name: "James King",
     title: "President and CEO",
+    image: jamesKing,
   },
   {
     name: "Julie Taylor",
     title: "VP of Marketing",
+    image: julieTaylor,
   },
   {
     name: "Eugene Lee",
     title: "CFO",
+    image: eugeneLee,
   },
   {
     name: "John Williams",
     title: "VP of Engineering",
+    image: johnWilliams,
   },
   {
     name: "Ray Moore",
     title: "VP of Sales",
+    image: rayMoore,
   },
   {
     name: "Paul Jones",
     title: "QA Manager",
+    image: paulJones,
   },
 ];
 
@@ -38,7 +48,7 @@ export default function EmployeeList() {
           key={employee.name}
           name={employee.name}
           title={employee.title}
-          image={employeeAvatar}
+          image={employee.image}
         />
       ))}
     </section>
